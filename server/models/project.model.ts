5d418ce8ab88b63fd4b27698458d3a307e1ad9c2
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-01-27 17:36:11 +0530

    project schema

--- server/models/project.model.ts
+++ server/models/project.model.ts
@@ -1,22 +1,22 @@
 import mongoose, { Schema } from "mongoose";
 
 const projectSchema = new Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: "User", 
-        required: true, 
-        unique: true
-    },
-    projectName: {
-        type: String, 
-        required: true
-    }, 
-    projectSlug: {
-        type: String,
-        unique: true,
-        required: true,
-    }
-})
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true,
+  },
+  projectName: {
+    type: String,
+    required: true,
+  },
+  projectSlug: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+});
 
 const Projects = mongoose.model("Projects", projectSchema);
 
