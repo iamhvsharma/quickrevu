b099c182d031607ca92797f4155dc5ad6492ca10
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-01-30 14:13:16 +0530

    Login Route

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import User from "../models/user.model";
+import jwt from "jsonwebtoken";
 
 // Initialize Router
 const router = express.Router();
@@ -24,6 +25,46 @@ router.post("/signup", async (req, res) => {
     res.status(401).send({
       msg: "Signup failed!",
       ERROR: error,
+    });
+  }
+});
+
+// Login Route
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({
+      email: email,
+    });
+
+    if (!user) {
+      res.status(404).send({
+        MSG: "User not found!",
+      });
+    } else {
+      const matchPassword = await bcrypt.compare(password, user?.password);
+      if (!matchPassword) {
+        res.status(401).send({
+          MSG: "Invalid credentials!",
+        });
+      } else {
+        const token = jwt.sign(
+          {
+            userId: user._id,
+          },
+          `process.env.JWT_SECRET`
+        );
+
+        res.status(201).send({
+          MSG: "Login Successful",
+          token: token,
+        });
+      }
+    }
+  } catch (error) {
+    res.status(500).send({
+      MSG: "Internal server error",
     });
   }
 });
