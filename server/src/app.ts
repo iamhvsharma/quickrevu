8f74ccfa5ae5dbb9433dc0e72f0476e1bed288f5
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-02-12 11:29:58 +0530

    Route added in app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import userRoutes from "./routes/user.route";
-
+import projectRoutes from "./routes/project.route"
 const app = express();
 
 // Middlewares
@@ -8,5 +8,6 @@ app.use(express.json());
 
 // API Call
 app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/new", projectRoutes)
 
 export { app };
