611a65568838f0cf3b7ac39e43b1377044d112cb
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-02-15 13:21:50 +0530

    Landing Page

---
+++ client/src/pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar"
+
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Landing Page</h1>
+    </div>
+  )
+}
+
+export default LandingPage
